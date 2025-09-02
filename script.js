5017737df922facb1cc43cf8b81071c79fa1a2e9
MohamedAmsakine17 <mohamedamsakine17@gmail.com>
2025-09-02 23:22:11 +0100

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-setTimeout(() => {
-    document.getElementById("header-img").src = "imgs/header.png";
+window.addEventListener("load", () => {
+  const headerImg = document.getElementById("header-img");
+
+  // First show the static image (already set as default in HTML)
+  headerImg.src = "imgs/header.png";
+
+  // Then show the gif
+  headerImg.src = "imgs/header.gif";
+
+  // After 2130ms switch back to static image
+  setTimeout(() => {
+    headerImg.src = "imgs/header.png";
   }, 2130);
+});
